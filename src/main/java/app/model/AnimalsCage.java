032506247d90599c8c4d5b6c5aa496862b444d7f
ingvar911 <ingvarkazan@gmail.com>
2023-06-06 15:21:40 +0300

    Тест прошел

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,38 @@
 package app.model;
 
+import app.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.sql.Time;
+
 @Component
+@Scope("prototype")
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    private Timer timer;
+
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,12 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("animalsCage")
+@Scope("singleton")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
